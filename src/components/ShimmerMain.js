35b2fce29ffc52ec7d194841e31406954d27e760
Utkarsh <tu858371@gmail.com>
2023-11-03 15:18:15 +0530

    Slight fixes

--- src/components/ShimmerMain.js
+++ src/components/ShimmerMain.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { SHIMMER_CARD_COUNT } from '../utils/constants';
 import ShimmerCard from './ShimmerCard';
+import { useSelector } from 'react-redux';
+
 
 const ShimmerMain = () => {
+
+  const isMenuOpen = useSelector(store => store.app.isMenuOpen);
   return (
-    <div className="w-11/12 mx-auto grid grid-cols-3">
+    <div
+      className={`grid ${
+        isMenuOpen
+          ? "md:grid-cols-2 xl:grid-cols-3"
+          : "sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+      }`}
+    >
       {Array(SHIMMER_CARD_COUNT)
         .fill("")
         .map((e, index) => (
